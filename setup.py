4254530ca2b6042e9af5ec33024b5b8c426bc744
Quan Wang <quanw@google.com>
2018-12-27 11:06:18 -0500

    Add LICENSE, setup, and manifest

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+VERSION = "2.1.1"
+
+with open("README.md", "r") as file_object:
+    LONG_DESCRIPTION = file_object.read()
+
+setuptools.setup(
+    name="repulsiveruby",
+    version=VERSION,
+    author="Quan Wang",
+    author_email="quanrpi@gmail.com",
+    description="RepulsiveRuby: A game made with Pygame",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wq2012/RepulsiveRuby",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
